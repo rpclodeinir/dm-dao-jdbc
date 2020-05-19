ff058f85d9cd657ac7f993a1410adb35b020c5ad
Clodeinir R. Peres <per.ferr04@gmail.com>
2020-05-19 16:19:52 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,20 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
+		
+		Seller seller = new Seller(21, "Bob Silva", "bob.silva@gmail.com", new Date(), 5000.0, obj);
+		
 		System.out.println(obj);
+		System.out.println(seller);
 
 	}
 
